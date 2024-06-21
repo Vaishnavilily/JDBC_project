6b5ee6ee5ab61736600128557d29189b343a68c0
Vaishnavi Vadlamani <110436750+Vaishnavilily@users.noreply.github.com>
2024-06-21 18:30:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,13 +14,13 @@ public class Main {
 // To Add New Items
         try{
             Connection con = DriverManager.getConnection(url, user, password);
-            Statement stat = con.createStatement();
+            Statement ps = con.createStatement();
             Scanner sc = new Scanner(System.in);
             System.out.print("Do you want to Enter New Products(Y/N)?");
             String input = sc.nextLine();
             if(input.equalsIgnoreCase("Y")) {
                 String choice = "Y";
-                for (int i = 0; choice.equals("Y"); i++) {
+                while(choice.equals("Y")) {
                     System.out.print("\nEnter the name of your product : ");
                     String name = sc.next();
                     System.out.print("\nEnter the price of your product : ");
@@ -30,9 +30,9 @@ public class Main {
                     System.out.print("\nEnter more data(Y/N):");
                     choice = sc.next();
                     String query1 = String.format("INSERT INTO product(name,price,quantity) VALUES('%s',%f,%d)", name, price, quantity);
-                    stat.addBatch(query1);
+                    ps.addBatch(query1);
                 }
-                int[] arr = stat.executeBatch();
+                int[] arr = ps.executeBatch();
                 for (int i = 0; i < arr.length; i++) {
                     if (arr[i] == 1) {
                         System.out.println("Product " + i + " added successfully");
@@ -41,13 +41,17 @@ public class Main {
                     }
                 }
             }
-       }
+            ps.close();
+        }
         catch(SQLException e){
             System.out.println(e.getMessage());
         }
 
 //To update/Modify products
         try{
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement ps = con.createStatement();
+            Scanner sc = new Scanner(System.in);
             System.out.print("Do you want to Update Information(Y/N):");
             String upd = sc.next();
             if(upd.equalsIgnoreCase("Y")) {
@@ -81,7 +85,7 @@ public class Main {
                         break;
                     }
                 }
-                int rowsAffected = stat.executeUpdate(query2);
+                int rowsAffected = ps.executeUpdate(query2);
                 if(rowsAffected>0){
                     System.out.println("Data Updated successfully");
                 }else{
@@ -95,6 +99,9 @@ public class Main {
 
  //To delete a product from inventory
         try{
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement ps = con.createStatement();
+            Scanner sc = new Scanner(System.in);
             System.out.print("Do you want to Delete Information(Y/N):");
             String del = sc.next();
             if(del.equalsIgnoreCase("Y")) {
@@ -102,7 +109,7 @@ public class Main {
                 int id = sc.nextInt();
                 String query3 = String.format("Delete from Product where id = %d", id);
 
-                int rowsAffected = stat.executeUpdate(query3);
+                int rowsAffected = ps.executeUpdate(query3);
                 if(rowsAffected>0){
                     System.out.println("Data Updated successfully");
                 }else{
@@ -115,29 +122,41 @@ public class Main {
         }
 
 // INVENTORY TRACKING
-    try{
-          int id = rs.getInt("id");
-          String name = rs.getString("name");
-          double price = rs.getDouble("price");
-          int quantity = rs.getInt("quantity");
-          
-          System.out.print("Selct Option:\n1. Display Details of All the Products in the Inventory \n2. View the Details of the Specific Product \nEnter Your Choice:");
-          int option = sc.nextInt();
-          if(option == 1){
-             String query4 = "SELECT * FROM PRODUCT";
-             ResultSet rs = stat.executeQuery(query4);
-             while(rs.next()){     
-                System.out.println(" ID:"+id+"\n NAME:"+name+"\n PRICE:"+price+"\n QUANTITY:"+quantity);
+        try{
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stat = con.createStatement();
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Select Option:\n1. Display Details of All the Products in the Inventory \n2. View the Details of the Specific Product \nEnter Your Choice:");
+            int option = sc.nextInt();
+            if(option == 1){
+                String query4 = "SELECT * FROM PRODUCT";
+                ResultSet rs = stat.executeQuery(query4);
+                while(rs.next()){
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    double price = rs.getDouble("price");
+                    int quantity = rs.getInt("quantity");
+                    System.out.println(" ID:"+id+"\n NAME:"+name+"\n PRICE:"+price+"\n QUANTITY:"+quantity);
+                }
             }
-          }
-        else{
-            System.out.print("Enter the ID of the Product to Display its Details from the Inventory: ");
-            int pro_id = sc.nextInt();
-            String query4 = String.format("SELECT * FROM INVENTORY WHERE id = %d",pro_id); 
-            System.out.println(" ID:"+id+"\n NAME:"+name+"\n PRICE:"+price+"\n QUANTITY:"+quantity);
+            else{
+                System.out.print("Enter the ID of the Product to Display its Details from the Inventory: ");
+                int pro_id = sc.nextInt();
+
+                String query4 = String.format("SELECT * FROM INVENTORY WHERE id = %d",pro_id);
+                ResultSet rs = stat.executeQuery(query4);
+                if(rs.next()){
+                  int id = rs.getInt("id");
+                  String name = rs.getString("name");
+                  double price = rs.getDouble("price");
+                  int quantity = rs.getInt("quantity");
+                  System.out.println(" ID:"+id+"\n NAME:"+name+"\n PRICE:"+price+"\n QUANTITY:"+quantity);
+                }else{
+                    System.out.println("Product not found");
+                }
+            }
         }
-      }
-    catch(SQLException e){
+        catch(SQLException e){
             System.out.println(e.getMessage());
         }
     }
